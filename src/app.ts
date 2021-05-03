d4ed4af17a5ce4a172f95b4f7f938f1025aad7f7
Joe Lyons <josephjlyons90@gmail.com>
2021-05-03 15:46:33 -0400

    initial commit | validation function for inputform

---
+++ src/app.ts
@@ -0,0 +1,138 @@
+//validation logic decorator
+
+interface IValidatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate(validatableInput: IValidatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+        isValid = isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (validatableInput.minLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null &&
+        typeof validatableInput.value === 'string') {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if(validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid &&  validatableInput.value >= validatableInput.min;
+    }
+    if(validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid &&  validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
+
+
+// autobind Decorator
+
+function autobind(
+    target: any,
+    methodName: string,
+    descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+//project input class 
+
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
+    titleInputEl: HTMLInputElement;
+    descriptionInputEl: HTMLInputElement;
+    peopleInputEl: HTMLInputElement;
+
+    constructor() {
+        this.templateElement = document.getElementById('project-input') as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+        this.element.id = 'user-input';
+
+        this.titleInputEl = this.element.querySelector('#title') as HTMLInputElement;
+        this.descriptionInputEl = this.element.querySelector('#description') as HTMLInputElement;
+        this.peopleInputEl = this.element.querySelector('#people') as HTMLInputElement;
+
+        this.configure()
+        this.attach()
+    }
+
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputEl.value;
+        const enteredDescription = this.descriptionInputEl.value;
+        const enteredPeople = this.peopleInputEl.value;
+
+        const titleValidatable: IValidatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable: IValidatable = {
+            value: enteredDescription,
+            required: true,
+            minLength: 5
+        };
+        const peopleValidatable: IValidatable = {
+            value: +enteredPeople,
+            required: true,
+            minLength: 1,
+            maxLength: 5
+            
+        };
+
+        if (
+            !validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)
+        ) {
+            alert('invalid input, try again')
+            return;
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
+    @autobind
+    private submitHandler(event: Event) {
+        event.preventDefault();
+        const userInput = this.gatherUserInput();
+        if (Array.isArray(userInput)) {
+            const [title, description, people] = userInput;
+            console.log(title, description, people)
+            this.clearInputs();
+        }
+    }
+
+    private clearInputs() {
+        this.titleInputEl.value = '';
+        this.descriptionInputEl.value = '';
+        this.peopleInputEl.value = '';
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler)
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    }
+}
+
+const projInput = new ProjectInput()
